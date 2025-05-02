e05e3c8730870960ae7d22de3e30f643d2252d27
Johana Gitari <johanagitari@gmail.com>
2025-05-02 12:50:49 +0300

    Final Touches

--- Routes/Router.js
+++ Routes/Router.js
@@ -12,7 +12,7 @@ let maxAge=10*60;
 //Login Routes
 router.get("/Login",(req,res)=>{
     const Message=""
-   res.render('/Login',{Message:Message})
+   res.render('Login',{Message:Message})
    })
 
 router.post("/Login",LoginController)
