31744c62260c50b66ee6899aa84f5f5935d59e83
Johana Gitari <johanagitari@gmail.com>
2025-05-02 10:08:32 +0300

    Final Touches

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
 app.use(express.static(__dirname + '/public'))
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(session({
     secret: 'keyboard cat',
